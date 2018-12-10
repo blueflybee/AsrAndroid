7b9de9a7d0b4ff2f6f50dcab82aba40c9a5f180b
shaojun <wusj2017@gmail.com>
2018-12-10 17:22:45 +0800

    引入javax

--- asrsdk/src/main/java/com/yjcloud/asrsdk/util/HttpCaller.java
+++ asrsdk/src/main/java/com/yjcloud/asrsdk/util/HttpCaller.java
@@ -12,58 +12,57 @@ import com.yjcloud.open.constant.HttpHeader;
 
 /**
  * 能力开放平台调用者
- * @author wangjq
  *
+ * @author wangjq
  */
 public class HttpCaller {
 
-	private String host;
+  private String host;
 
-	private String accesskeyId;
+  private String accesskeyId;
 
-	private String accessKeySecret;
+  private String accessKeySecret;
 
-	private static final int TIMEOUT_MILLS = 5000;
-
-
-
-	public HttpCaller(String host, String accesskeyId, String accessKeySecret) {
-		this.host = host;
-		this.accesskeyId = accesskeyId;
-		this.accessKeySecret = accessKeySecret;
-
-	}
+  private static final int TIMEOUT_MILLS = 5000;
 
 
-	/**
-	 *
-	 * @param contentType
-	 * @param body
-	 * @return
-	 * @throws Exception
-	 */
-	public HttpResponse sendPost(String contentType, String path, String body) throws Exception{
-		Request request = new Request("POST",host,path,accesskeyId,accessKeySecret,TIMEOUT_MILLS);
-		Map<String, String> headMap = new HashMap<String, String>();
-		// 设置body 的content-type
-		headMap.put(
-			HttpHeader.HTTP_HEADER_CONTENT_TYPE,
-			contentType
-		);
-		request.setHeaders(headMap);
-		request.setStringBody(body);
-		HttpResponse response = Client.execute(request);
-		return response;
-	}
+  public HttpCaller(String host, String accesskeyId, String accessKeySecret) {
+    this.host = host;
+    this.accesskeyId = accesskeyId;
+    this.accessKeySecret = accessKeySecret;
 
-	/**
-	 * content-type 默认为 application/json
-	 * @param path
-	 * @param body
-	 * @return
-	 * @throws Exception
-	 */
-	public HttpResponse sendPost(String path, String body) throws Exception{
-		return sendPost(ContentType.CONTENT_TYPE_JSON,path,body);
-	}
+  }
+
+
+  /**
+   * @param contentType
+   * @param body
+   * @return
+   * @throws Exception
+   */
+  public HttpResponse sendPost(String contentType, String path, String body) throws Exception {
+    Request request = new Request("POST", host, path, accesskeyId, accessKeySecret, TIMEOUT_MILLS);
+    Map<String, String> headMap = new HashMap<String, String>();
+    // 设置body 的content-type
+    headMap.put(
+        HttpHeader.HTTP_HEADER_CONTENT_TYPE,
+        contentType
+    );
+    request.setHeaders(headMap);
+    request.setStringBody(body);
+    HttpResponse response = Client.execute(request);
+    return response;
+  }
+
+  /**
+   * content-type 默认为 application/json
+   *
+   * @param path
+   * @param body
+   * @return
+   * @throws Exception
+   */
+  public HttpResponse sendPost(String path, String body) throws Exception {
+    return sendPost(ContentType.CONTENT_TYPE_JSON, path, body);
+  }
 }
