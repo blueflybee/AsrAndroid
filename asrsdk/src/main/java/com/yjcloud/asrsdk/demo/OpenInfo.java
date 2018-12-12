221a2ba29c99b8c1ca11bc4685a9be13555feba6
shaojun <wusj2017@gmail.com>
2018-12-12 11:56:43 +0800

    所有测试通过

--- asrsdk/src/main/java/com/yjcloud/asrsdk/demo/OpenInfo.java
+++ asrsdk/src/main/java/com/yjcloud/asrsdk/demo/OpenInfo.java
@@ -17,5 +17,61 @@ public class OpenInfo {
 	public static String model_path = "/models";
 	
 	public static String sdk_server_path = "/serverinfo";
-	
+
+
+	public static String getHost() {
+		return host;
+	}
+
+	public static void setHost(String host) {
+		OpenInfo.host = host;
+	}
+
+	public static String getAk_id() {
+		return ak_id;
+	}
+
+	public static void setAk_id(String ak_id) {
+		OpenInfo.ak_id = ak_id;
+	}
+
+	public static String getAk_secret() {
+		return ak_secret;
+	}
+
+	public static void setAk_secret(String ak_secret) {
+		OpenInfo.ak_secret = ak_secret;
+	}
+
+	public static String getVocab_path() {
+		return vocab_path;
+	}
+
+	public static void setVocab_path(String vocab_path) {
+		OpenInfo.vocab_path = vocab_path;
+	}
+
+	public static String getCvocab_path() {
+		return cvocab_path;
+	}
+
+	public static void setCvocab_path(String cvocab_path) {
+		OpenInfo.cvocab_path = cvocab_path;
+	}
+
+	public static String getModel_path() {
+		return model_path;
+	}
+
+	public static void setModel_path(String model_path) {
+		OpenInfo.model_path = model_path;
+	}
+
+	public static String getSdk_server_path() {
+		return sdk_server_path;
+	}
+
+	public static void setSdk_server_path(String sdk_server_path) {
+		OpenInfo.sdk_server_path = sdk_server_path;
+	}
 }
